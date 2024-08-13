09258df66d27c135421112adb7dde3de3ce08c32
ted <theo_drz@live.com>
2024-08-12 23:41:29 -0400

    updated styles for LinkButtons and RepoList components

--- src/components/subcomponents/LinkButtons.ts
+++ src/components/subcomponents/LinkButtons.ts
@@ -10,11 +10,13 @@ export default function LinkButtons({
   pageUrl?: string,
   addClasses?: string
 }) {
-  const sharedClasses = 'flex-1 text-center p-4 flex justify-center items-center gap-2 text-white rounded-xl transition-colors duration-300';
+  // const sharedClasses = 'flex-1 text-center p-4 flex justify-center items-center gap-2 text-white rounded-xl transition-colors duration-300';
+  const sharedClasses = 'font-bold text-blue-600 border-4 border-blue-600 hover:bg-blue-600 hover:text-white hover:text-xl flex-1 text-center p-4 flex justify-center items-center gap-2 rounded-xl transition-all duration-500';
   return (
     t('div', { className: 'flex-1 flex flex-wrap p-2 gap-2 bg-white' + (addClasses ? ' ' + addClasses : '') }, [
       LinkTo({
-        className: `${sharedClasses} text-white bg-orange-500 font-bold`,
+        // className: `${sharedClasses} text-white bg-orange-500 font-bold`,
+        className: sharedClasses,
         content: {
           href: codeUrl,
           name: 'View Code',
@@ -24,7 +26,8 @@ export default function LinkButtons({
       }),
       !pageUrl ? undefined :
         LinkTo({
-          className: `${sharedClasses} text-white bg-rose-500 font-bold`,
+          // className: `${sharedClasses} text-white bg-rose-500 font-bold`,
+          className: sharedClasses,
           content: {
             href: pageUrl,
             name: 'View Page',
--- src/components/subcomponents/RepoList.ts
+++ src/components/subcomponents/RepoList.ts
@@ -9,7 +9,7 @@ export default function RepoList({ repos, title }: { repos: Repo[], title: strin
       return (
         t('div', { className: 'flex flex-col lg:flex-row mx-16 mb-6 bg-white rounded-xl' }, [
           t('div', { className: 'flex-1 p-4 flex justify-between items-center gap-4 text-center' }, [
-            t('h1', { className: 'flex-1 text-center text-lg', textContent: repo.name }),
+            t('h1', { className: 'flex-1 text-center text-lg font-medium', textContent: repo.name }),
             t('div', { className: 'flex' }, 
               LanguageIconLinks({ repo })
             )
